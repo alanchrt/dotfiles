0f21d98003bed9773b9ebea3f58d162c33599f39
Alan Christopher Thomas <alan@alanct.com>
2016-06-22 09:35:32 -0500

    simplify dotme script, remove installs (now in ansible)

--- dotme.sh
+++ dotme.sh
@@ -1,28 +1,15 @@
 #!/usr/bin/env bash
 
-backup_file() {
-    test -e $HOME/$1.dotbackup || test -e $HOME/$1 && cp -LR $HOME/$1 $HOME/$1.dotbackup && rm -rf $HOME/$1
-    true
-}
-
-backup_directory() {
-    test -d $HOME/$1.dotbackup || test -d $HOME/$1 && cp -LR $HOME/$1 $HOME/$1.dotbackup && rm -rf $HOME/$1
-    true
-}
-
 link_file() {
-    backup_file $2
-    ln -s `pwd`/confs/$1 $HOME/$2
+    ln -s `pwd`/home/$1 $HOME/$2
 }
 
 link_directory() {
-    backup_directory $2
-    ln -s `pwd`/confs/$1 $HOME/$2
+    ln -s `pwd`/home/$1 $HOME/$2
 }
 
 copy_file() {
-    backup_file $2
-    cp `pwd`/confs/$1 $HOME/$2
+    cp `pwd`/home/$1 $HOME/$2
 }
 
 clone_or_pull_repo() {
@@ -37,13 +24,12 @@ clone_or_pull_repo() {
 
 install_oh_my_zsh() {
     echo "Installing oh-my-zsh..."
-    if pushd $HOME/.oh-my-zsh; then git pull origin master; popd; else git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh; fi
+    clone_or_pull_repo robbyrussell/oh-my-zsh .oh-my-zsh
 }
 
 install_spacemacs() {
     echo "Installing spacemacs..."
-    test -e $HOME/.emacs.d/init.el || backup_directory .emacs.d
-    if pushd $HOME/.emacs.d; then git pull origin master; popd; else git clone https://github.com/syl20bnr/spacemacs $HOME/.emacs.d; fi
+    clone_or_pull_repo syl20nbr/spacemacs .emacs.d
 }
 
 configure_shell() {
@@ -71,8 +57,7 @@ configure_zsh() {
 
 configure_git() {
     echo "Configuring git..."
-    backup_file .gitconfig
-    sed -e 's/\[\[GIT_NAME\]\]/'"$GIT_NAME"'/g' -e 's/\[\[GIT_EMAIL\]\]/'"$GIT_EMAIL"'/g' `pwd`/confs/.gitconfig-global > $HOME/.gitconfig
+    sed -e 's/\[\[GIT_NAME\]\]/'"$GIT_NAME"'/g' -e 's/\[\[GIT_EMAIL\]\]/'"$GIT_EMAIL"'/g' `pwd`/home/.gitconfig-global > $HOME/.gitconfig
     copy_file .gitignore-global .gitignore
 }
 
@@ -81,46 +66,12 @@ configure_emacs() {
     link_file .spacemacs .spacemacs
 }
 
-configure_tmux() {
-    echo "Configuring tmux..."
-    link_file .tmux.conf .tmux.conf
-}
-
-delete_backups() {
-    rm -rf $HOME/.bash_aliases.dotbackup
-    rm -rf $HOME/.bash_local.dotbackup
-    rm -rf $HOME/.bash_profile.dotbackup
-    rm -rf $HOME/.bashrc.dotbackup
-    rm -rf $HOME/.gitconfig.dotbackup
-    rm -rf $HOME/.gitignore.dotbackup
-    rm -rf $HOME/.shenv.dotbackup
-#    rm -rf $HOME/.tmux.conf.dotbackup
-    rm -rf $HOME/.zshrc.dotbackup
-    rm -rf $HOME/.emacs.d.dotbackup
-}
-
 set -e
-
-while :
-do
-    case $1 in
-        -h | --help | -\?) echo "See https://github.com/alanctkc/dotfiles/blob/master/README.md"; exit 0; ;;
-        --delete-backups) DELETE_BACKUPS=1; shift; ;;
-        --) shift; break; ;;
-        -*) printf >&2 'WARNING: Unknown option (ignored): %s\n' "$1"; shift; ;;
-        *) break; ;;
-    esac
-done
-
-if [ "$DELETE_BACKUPS" == 1 ]; then
-    delete_backups
-    echo "Backups deleted."
-    exit 0
-fi
 
 if [ -z "$GIT_NAME" ]; then
     read -p "Git user.name: " GIT_NAME
 fi
+
 if [ -z "$GIT_EMAIL" ]; then
     read -p "Git user.email: " GIT_EMAIL
 fi
@@ -131,7 +82,6 @@ configure_shell
 configure_bash
 configure_zsh
 configure_git
-#configure_tmux
 configure_emacs
 
 echo "Done."
