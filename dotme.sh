dad72f5a3097077717f158e08dc54f96c269996b
Alan Christopher Thomas <alan@alanct.com>
2016-08-12 09:46:53 -0500

    add user-dirs to dotfiles

--- dotme.sh
+++ dotme.sh
@@ -98,6 +98,11 @@ configure_i3() {
     link_file .config/dunst/dunstrc .config/dunst/dunstrc
 }
 
+configure_desktop() {
+    echo "Configuring Desktop settings..."
+    link_file .config/user-dirs.dirs .config/user-dirs.dirs
+}
+
 configure_vimperator() {
     echo "Configuring Vimperator..."
     link_file .vimperatorrc .vimperatorrc
@@ -116,6 +121,7 @@ configure_git
 configure_emacs
 configure_python
 configure_i3
+configure_desktop
 configure_vimperator
 
 touch $HOME/.dotted
