5996dc9390bf2101b44ec4eb4d81da0fbf825187
Alan Christopher Thomas <alan@alanct.com>
2016-08-12 22:01:42 -0500

    write script to move between workspaces constrained by the current output

---
+++ home/.config/i3/workspaceonoutput.py
@@ -0,0 +1,30 @@
+import i3
+import sys
+
+from collections import defaultdict
+
+command = sys.argv[1]
+direction = sys.argv[2]
+
+outputs = defaultdict(list)
+
+for workspace in i3.get_workspaces():
+    output = outputs[workspace['output']]
+    output.append(workspace)
+    if workspace['focused']:
+        current_workspace = workspace
+        index_on_output = len(output) - 1
+
+target_output = outputs[current_workspace['output']]
+workspace_count = len(target_output)
+if direction == 'prev':
+    target_workspace = target_output[index_on_output - 1 % workspace_count]
+elif direction == 'next':
+    target_workspace = target_output[index_on_output + 1 % workspace_count]
+
+if command == 'workspace':
+    i3.command('workspace', target_workspace['name'])
+elif command == 'move':
+    i3.command('move', 'container', 'to', 'workspace', target_workspace['name'])
+    i3.command('workspace', target_workspace['name'])
+    i3.command('focus')
