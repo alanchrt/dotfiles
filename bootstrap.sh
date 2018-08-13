24c328c07208aeb08bf3f40b26e2f5bf0b974e0f
Alan Christopher Thomas <alan@tentmaker.co>
2018-08-13 11:14:12 -0500

    Name remote `alanctkc` so we don't monkey with the user's origin

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,10 +15,10 @@ install_dotfiles() {
     if [ ! -d /home/alan/.config/dotfiles ]; then
         su alan -c 'git clone https://github.com/alanctkc/dotfiles.git /home/alan/.config/dotfiles'
     fi
-    su alan -c 'git -C /home/alan/.config/dotfiles remote rm origin'
-    su alan -c 'git -C /home/alan/.config/dotfiles remote add origin git@github.com:alanctkc/dotfiles.git'
+    su alan -c 'git -C /home/alan/.config/dotfiles remote rm alanctkc || true'
+    su alan -c 'git -C /home/alan/.config/dotfiles remote add alanctkc git@github.com:alanctkc/dotfiles.git'
     # TODO NIXOS-BRANCH delete following two lines after master merge
-    su alan -c 'git -C /home/alan/.config/dotfiles fetch origin nixos'
+    su alan -c 'git -C /home/alan/.config/dotfiles fetch alanctkc nixos'
     su alan -c 'git -C /home/alan/.config/dotfiles checkout nixos'
     rm /etc/nixos/configuration.nix
     ln -sf /home/alan/.config/dotfiles/configuration.nix /etc/nixos/configuration.nix
