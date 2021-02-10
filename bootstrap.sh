00606dc3e1d6d699a5e8b3d1e73237f8ca4a527e
Alan Christopher Thomas <alan@chrt.me>
2021-02-10 10:41:06 -0700

    Add chezmoi apply

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,5 +8,6 @@ sudo apt install --assume-yes software-properties-common python-is-python3 ansib
 wget --no-clobber https://github.com/twpayne/chezmoi/releases/download/v1.8.11/chezmoi_1.8.11_linux_amd64.deb -O /tmp/chezmoi.deb
 sudo apt install /tmp/chezmoi.deb
 
-# init chezmoi
+# init and apply chezmoi
 chezmoi init "git@github.com:alanchrt/dotfiles.git#regolith"
+chezmoi apply
