900aae353e8395e8dd96c850056d07c3ca31e03a
Alan Christopher Thomas <alan@chrt.me>
2021-02-10 08:44:40 -0700

    Clean slate

--- home/.local/share/highlight.el
+++
@@ -1,3 +0,0 @@
-(mapc 'load (file-expand-wildcards "~/.emacs.d/elpa/base16-theme-*/base16-theme.el"))
-(mapc 'load (file-expand-wildcards "~/.emacs.d/elpa/base16-theme-*/base16-tomorrow-night-theme.el"))
-(mapc 'load (file-expand-wildcards "~/.emacs.d/elpa/e2ansi-*/bin/e2ansi-cat"))
