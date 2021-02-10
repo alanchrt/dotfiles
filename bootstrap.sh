e6514e2f71dbf9dc20828a76c39155928d399a9a
Alan Christopher Thomas <alan@alanct.com>
2021-02-10 11:37:43 -0700

    Manually clone dotfiles (branch didn't work in chezmoi)

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,12 +2,13 @@
 
 # install ansible
 sudo apt upgrade --assume-yes
-sudo apt install --assume-yes software-properties-common python-is-python3 ansible
+sudo apt install --assume-yes git software-properties-common python-is-python3 ansible
 
 # install chezmoi
 wget --no-clobber https://github.com/twpayne/chezmoi/releases/download/v1.8.11/chezmoi_1.8.11_linux_amd64.deb -O /tmp/chezmoi.deb
 sudo apt install /tmp/chezmoi.deb
 
 # init and apply chezmoi
-chezmoi init "git@github.com:alanchrt/dotfiles.git#regolith"
+git clone -b regolith git@github.com:alanchrt/dotfiles.git ~/.local/share/chezmoi
+chmod 0700 ~/.local/share/chezmoi/
 chezmoi apply
