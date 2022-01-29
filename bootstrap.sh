876d5ffe8595d7d1208df5cd85e5845ee79e7329
Alan Christopher Thomas <alan@chrt.me>
2022-01-28 21:19:11 -0700

    Update bootstrap for master branch

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,14 +19,14 @@ install /tmp/dotfiles/chezmoi $HOME/.local/bin
 
 # init and apply chezmoi
 if [ ! -d $HOME/Projects/dotfiles ] ; then
-    git clone -b fedora git@github.com:alanchrt/dotfiles.git $HOME/Projects/dotfiles
+    git clone git@github.com:alanchrt/dotfiles.git $HOME/Projects/dotfiles
 fi
 chezmoi apply --source $HOME/Projects/dotfiles
 
 # install ansible galaxy dependencies
 ansible-galaxy collection install community.general
 if [ ! -f /tmp/dotfiles/requirements.yml ] ; then
-    wget --no-clobber -O /tmp/dotfiles/requirements.yml https://raw.githubusercontent.com/alanchrt/dotfiles/fedora/requirements.yml
+    wget --no-clobber -O /tmp/dotfiles/requirements.yml https://raw.githubusercontent.com/alanchrt/dotfiles/master/requirements.yml
 fi
 ansible-galaxy install -r /tmp/dotfiles/requirements.yml
 
@@ -34,6 +34,6 @@ ansible-galaxy install -r /tmp/dotfiles/requirements.yml
 rm -r /tmp/dotfiles
 
 # run ansible playbook
-ANSIBLE_FORCE_COLOR=true ansible-pull --checkout fedora --url git@github.com:alanchrt/dotfiles.git -i hosts --extra-vars "ansible_sudo_pass=$PASSWORD"
+ANSIBLE_FORCE_COLOR=true ansible-pull --checkout master --url git@github.com:alanchrt/dotfiles.git -i hosts --extra-vars "ansible_sudo_pass=$PASSWORD"
 
 echo "Please restart this machine to make sure all groups, extensions, and services reload properly."
