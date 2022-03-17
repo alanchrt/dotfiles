500dde04e2e66542e04315c0848a0145fdd8209a
Alan Christopher Thomas <alan@chrt.me>
2022-03-17 09:35:58 -0600

    Switch to helm for fuzzy search

--- dot_doom.d/init.el
+++ dot_doom.d/init.el
@@ -21,10 +21,10 @@
 
        :completion
        company           ; the ultimate code completion backend
-       ;;helm              ; the *other* search engine for love and life
+       (helm +fuzzy +icons); the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
-       (vertico +icons)  ; the search engine of the future
+       ;;(vertico +icons)  ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
